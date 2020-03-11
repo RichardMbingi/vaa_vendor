76b4cfd143bf694c7d9123cab383ce7fa0f6e411
Richard Mbingi <richard.mbingi@strathmore.edu>
2020-03-11 11:51:17 +0300

    Fix sign up errors

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,27 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const express = require('express');
-const app = express();
+const app = require('express')();
 
 admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello world!");
-});
+const config = {
+    apiKey: "AIzaSyCE_0SaFpObRfRW3gI6YuL7ttCcay4iU9s",
+    authDomain: "avatar-fashion-90125.firebaseapp.com",
+    databaseURL: "https://avatar-fashion-90125.firebaseio.com",
+    projectId: "avatar-fashion-90125",
+    storageBucket: "avatar-fashion-90125.appspot.com",
+    messagingSenderId: "451283469735",
+    appId: "1:451283469735:web:cff22b5382b16efe8ba282",
+    measurementId: "G-4P7QRYZ98F"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get('/items', (req, res) => {
-    admin.firestore().collection('items')
+    db.collection('items')
         .orderBy('createdAt', 'desc')
         .get()
         .then(data => {
@@ -35,8 +46,7 @@ app.post('/item', (req, res) => {
         createdAt: new Date().toISOString()
     };
 
-    admin.firestore()
-        .collection('items')
+    db.collection('items')
         .add(newItem)
         .then(doc => {
             res.json({
@@ -49,6 +59,62 @@ app.post('/item', (req, res) => {
             });
             console.error(err);
         })
+});
+
+//Sign Up route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    //TODO: Validate users
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if (doc.exists) {
+                return res.status(400).json({
+                    handle: 'This handle is already taken'
+                });
+            } else {
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(newUser.email, newUser.password);
+            }
+        })
+        .then((data) => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then((idToken) => {
+            token = idToken;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId
+            };
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({
+                token
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+            if (err.code === 'auth/email-already-in-use') {
+                return res.status(400).json({
+                    email: 'Email is already registered'
+                });
+            } else {
+                return res.status(500).json({
+                    error: err.code
+                });
+            }
+        });
 });
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
