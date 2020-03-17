62e74f68eba389a6baa9c09720ebbafa1a0d4447
Richard Mbingi <richard.mbingi@strathmore.edu>
2020-03-17 14:03:47 +0300

    Code refactoring

--- functions/index.js
+++ functions/index.js
@@ -1,183 +1,19 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+
 const app = require('express')();
 
-admin.initializeApp();
+const FBAuth = require('./util/fbAuth');
 
-const config = {
-    apiKey: "AIzaSyCE_0SaFpObRfRW3gI6YuL7ttCcay4iU9s",
-    authDomain: "avatar-fashion-90125.firebaseapp.com",
-    databaseURL: "https://avatar-fashion-90125.firebaseio.com",
-    projectId: "avatar-fashion-90125",
-    storageBucket: "avatar-fashion-90125.appspot.com",
-    messagingSenderId: "451283469735",
-    appId: "1:451283469735:web:cff22b5382b16efe8ba282",
-    measurementId: "G-4P7QRYZ98F"
-};
+const { getAllItems, postOneItem } = require('./handlers/items');
+const { signup, login} = require('./handlers/users');
 
-const firebase = require('firebase');
-firebase.initializeApp(config);
+//?Item routes
+app.get('/items', getAllItems);
+app.post('/item', FBAuth, postOneItem);
 
-const db = admin.firestore();
-
-app.get('/items', (req, res) => {
-    db.collection('items')
-        .orderBy('createdAt', 'desc')
-        .get()
-        .then(data => {
-            let items = [];
-            data.forEach(doc => {
-                items.push({
-                    itemId: doc.id,
-                    body: doc.data().body,
-                    userHandle: doc.data().userHandle,
-                    createdAt: doc.data().createdAt,
-                });
-            });
-            return res.json(items);
-        })
-        .catch(err => console.error(err));
-})
-
-app.post('/item', (req, res) => {
-    const newItem = {
-        body: req.body.body,
-        userHandle: req.body.userHandle,
-        createdAt: new Date().toISOString()
-    };
-
-    db.collection('items')
-        .add(newItem)
-        .then(doc => {
-            res.json({
-                message: `document ${doc.id} created successfully`
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: 'Umebunt brathe'
-            });
-            console.error(err);
-        })
-});
-
-const isEmpty = (string) => {
-    if (string.trim() === '') return true;
-    else return false;
-}
-
-const isEmail = (email) => {
-    if (email.match(regEx)) return true;
-    else return false;
-}
-
-//Sign Up route
-app.post('/signup', (req, res) => {
-    const newUser = {
-        email: req.body.email,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
-        handle: req.body.handle,
-    };
-
-    let errors = {};
-
-    if (isEmpty(newUser.email)) {
-        errors.email = 'Email must not be empty'
-    } else if (!isEmail(newUser.email)) {
-        errors.email = 'Must be a valid email address'
-    }
-
-    if (isEmpty(newUser.password)) errors.password = 'Must not be empty'
-    if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
-    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
-
-    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-    //TODO: Validate users
-    let token, userId;
-    db.doc(`/users/${newUser.handle}`).get()
-        .then(doc => {
-            if (doc.exists) {
-                return res.status(400).json({
-                    handle: 'This handle is already taken'
-                });
-            } else {
-                return firebase
-                    .auth()
-                    .createUserWithEmailAndPassword(newUser.email, newUser.password);
-            }
-        })
-        .then((data) => {
-            userId = data.user.uid;
-            return data.user.getIdToken();
-        })
-        .then((idToken) => {
-            token = idToken;
-            const userCredentials = {
-                handle: newUser.handle,
-                email: newUser.email,
-                createdAt: new Date().toISOString(),
-                userId
-            };
-            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-        })
-        .then(() => {
-            return res.status(201).json({
-                token
-            });
-        })
-        .catch((err) => {
-            console.error(err);
-            if (err.code === 'auth/email-already-in-use') {
-                return res.status(400).json({
-                    email: 'Email is already registered'
-                });
-            } else {
-                return res.status(500).json({
-                    error: err.code
-                });
-            }
-        });
-});
-
-app.post('/login', (req, res) => {
-    const user = {
-        email: req.body.email,
-        password: req.body.password
-    };
-
-    let errors = {};
-
-    if (isEmpty(user.email)) errors.email = 'Must not be empty';
-    if (isEmpty(user.password)) errors.password = 'Must not be empty';
-
-    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
-        .then(data => {
-            return data.user.getIdToken();
-        })
-        .then(token => {
-            return res.json({
-                token
-            })
-        })
-        .catch(err => {
-            console.error(err);
-            if (err.code === 'auth/wrong-password') {
-                return res.status(403).json({
-                    general: 'Wrong credentials, please try again'
-                })
-            } else {
-                return res.status(500).json({
-                    error: err.code
-                });
-            }
-
-        })
-})
+//?Users routes
+app.post('/signup', signup);
+app.post('/login', login)
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
 
-const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCE_0SaFpObRfRW3gI6YuL7ttCcay4iU9s",
+    authDomain: "avatar-fashion-90125.firebaseapp.com",
+    databaseURL: "https://avatar-fashion-90125.firebaseio.com",
+    projectId: "avatar-fashion-90125",
+    storageBucket: "avatar-fashion-90125.appspot.com",
+    messagingSenderId: "451283469735",
+    appId: "1:451283469735:web:cff22b5382b16efe8ba282",
+    measurementId: "G-4P7QRYZ98F"
+};
